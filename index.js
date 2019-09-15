e4d63ba16ed2714d03b811aa45c35e80debf6ed4
JuniorDuarte93 <jr.duartee.93@gmail.com>
2019-09-14 23:45:08 -0300

    'pronto'

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const calcPegadaCarbono = require('./calcPegadaCarbono')
+
+//app.use(bodyParser.text())
+//app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.post('/pegadacarbono', (req, res) => {
+
+    result = calcPegadaCarbono(
+        req.body.energiaKwhMes,
+        req.body.gasCozinhaKgMes,
+        req.body.gasNaturalM3Mes,
+        req.body.veicPGasolinaKmMes,
+        req.body.veicMGasolinaKmMes,
+        req.body.veicGGasolinaKmMes,
+        req.body.motoGasolinaKmMes,
+        req.body.veicPEtanolKmMes,
+        req.body.veicMEtanolKmMes,
+        req.body.veicGEtanolKmMes,
+        req.body.motoEtanolKmMes,
+        req.body.carroDieselKmMes,
+        req.body.onibusDieselUrbanoKmMes,
+        req.body.onibusDieselRodoviaKmMes,
+        req.body.carroGnvKmMes,
+        req.body.viagemNacionalKmMes,
+        req.body.viagemInternacionalKmMes,
+        req.body.aterroResiduosKgDia
+    )
+    res.send(result)
+})
+
+app.listen(3000, () => {
+    console.log('Iniciado')
+})
